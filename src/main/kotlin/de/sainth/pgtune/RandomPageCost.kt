32a11fbf19940729dc77aa196c8ecbdb463f725b
Tobias Wink <sainth@sainth.de>
2019-10-09 06:37:09 +0200

    Add unit tests for RandomPageCost

--- src/main/kotlin/de/sainth/pgtune/RandomPageCost.kt
+++ src/main/kotlin/de/sainth/pgtune/RandomPageCost.kt
@@ -1,7 +1,7 @@
 package de.sainth.pgtune
 
 class RandomPageCost(systemConfiguration: SystemConfiguration) : PgConfigurationParameter("random_page_cost") {
-    private val randomPageCost: Double = when (systemConfiguration.dataStorage) {
+    val randomPageCost: Double = when (systemConfiguration.dataStorage) {
         DataStorage.HDD -> 4.0
         DataStorage.SSD -> 1.1
         DataStorage.SAN -> 1.1
