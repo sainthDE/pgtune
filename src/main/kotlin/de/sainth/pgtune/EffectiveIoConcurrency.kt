823721d7619215095c12dc682bde911f25a3a3dc
Tobias Wink <sainth@sainth.de>
2019-10-09 06:39:29 +0200

    Add unit tests for EffectiveIoConcurrency

--- src/main/kotlin/de/sainth/pgtune/EffectiveIoConcurrency.kt
+++ src/main/kotlin/de/sainth/pgtune/EffectiveIoConcurrency.kt
@@ -1,7 +1,7 @@
 package de.sainth.pgtune
 
 class EffectiveIoConcurrency(systemConfiguration: SystemConfiguration) : PgConfigurationParameter("effective_io_concurrency") {
-    private val effectiveIoConcurrency: Int = when(systemConfiguration.dataStorage) {
+    val effectiveIoConcurrency: Int = when(systemConfiguration.dataStorage) {
         DataStorage.HDD -> 2
         DataStorage.SSD -> 200
         DataStorage.SAN -> 300
