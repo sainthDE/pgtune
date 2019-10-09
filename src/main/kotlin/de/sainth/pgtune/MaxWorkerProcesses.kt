4775c2531ec2d0ceef248c944fc92f0fd3c93755
Tobias Wink <sainth@sainth.de>
2019-10-09 06:49:45 +0200

    Add unit tests for MaxWorkerProcesses

--- src/main/kotlin/de/sainth/pgtune/MaxWorkerProcesses.kt
+++ src/main/kotlin/de/sainth/pgtune/MaxWorkerProcesses.kt
@@ -1,12 +1,10 @@
 package de.sainth.pgtune
 
 class MaxWorkerProcesses(systemConfiguration: SystemConfiguration) : PgConfigurationParameter("max_worker_processes") {
-    private val maxWorkerProcesses: Int = systemConfiguration.cpus?.toInt() ?: 8
+    val maxWorkerProcesses: Int = systemConfiguration.cpus?.toInt() ?: 8
 
     init {
-        if(systemConfiguration.dbVersion == PostgresVersion.V9_4) {
-            throw IllegalArgumentException("$name is not supported by PostgreSQL version ${systemConfiguration.dbVersion.version}")
-        }
+        require(systemConfiguration.dbVersion != PostgresVersion.V9_4) { "$name is not supported by PostgreSQL version ${systemConfiguration.dbVersion.version}" }
     }
 
     override fun getParameterString(): String {
