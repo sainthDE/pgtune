393e19812122ba9b60313b196c1f33bf5c4d57bd
Tobias Wink <sainth@sainth.de>
2019-10-10 20:35:27 +0200

    Bump versions to 1.0

--- src/main/kotlin/de/sainth/pgtune/Application.kt
+++ src/main/kotlin/de/sainth/pgtune/Application.kt
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.License
 @OpenAPIDefinition(
     info = Info(
             title = "pgtune",
-            version = "0.0",
+            version = "1.0",
             description = "A service to generate some optimized configuration parameters for PostgreSQL based on best practices.",
             license = License(name = "MIT", url = "https://opensource.org/licenses/MIT"),
             contact = Contact(url = "https://sainth.de", name = "Tobias Wink", email = "sainth@sainth.de")
