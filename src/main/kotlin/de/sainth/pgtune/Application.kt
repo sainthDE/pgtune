8b455e6a79a89a18a3d285089230808be81cce8d
Tobias Wink <tobias.wink@hs-karlsruhe.de>
2020-11-03 08:41:29 +0100

    Reenable swagger

--- src/main/kotlin/de/sainth/pgtune/Application.kt
+++ src/main/kotlin/de/sainth/pgtune/Application.kt
@@ -1,27 +1,27 @@
 package de.sainth.pgtune
 
 import io.micronaut.runtime.Micronaut.*
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition
-//import io.swagger.v3.oas.annotations.info.Contact
-//import io.swagger.v3.oas.annotations.info.Info
-//import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 
-//@OpenAPIDefinition(
-//    info = Info(
-//            title = "pgtune",
-//            version = "1.0",
-//            description = "A service to generate some optimized configuration parameters for PostgreSQL based on best practices.",
-//            license = License(name = "MIT", url = "https://opensource.org/licenses/MIT"),
-//            contact = Contact(url = "https://sainth.de", name = "Tobias Wink", email = "sainth@sainth.de")
-//    )
-//)
-//object Application {
-
+@OpenAPIDefinition(
+    info = Info(
+            title = "pgtune",
+            version = "1.0",
+            description = "A service to generate some optimized configuration parameters for PostgreSQL based on best practices.",
+            license = License(name = "MIT", url = "https://opensource.org/licenses/MIT"),
+            contact = Contact(url = "https://sainth.de", name = "Tobias Wink", email = "sainth@sainth.de")
+    )
+)
+object Application {
+    @JvmStatic
     fun main(args: Array<String>) {
         build()
                 .args(*args)
                 .packages("de.sainth.pgtune")
-//                .mainClass(Application.javaClass)
+                .mainClass(Application.javaClass)
                 .start()
     }
-//}
+}
